7ba540ec7b1c06c0dc2632e46369f0a01505d602
ΛNDRMDΛ <recover4suhayb@gmail.com>
2026-03-19 16:51:50 +0000

    Fixed images

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<img src="$lib/assets/vmshade.png" alt="vmshade logo" width="50" height="50" class="logo" />
+<img src="/vmshade.png" alt="vmshade logo" width="50" height="50" class="logo" />
 
 <h3>Hey, I'm vmshade</h3>
 
@@ -8,7 +8,7 @@
 		color: rgb(255, 255, 255);
 		margin-top: 0px;
 		text-align: left;
-		margin-bottom: 0px;S
+		margin-bottom: 0px;
 	}
 	.logo {
 		margin-top: 200px;
